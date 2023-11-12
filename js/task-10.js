aef9503879442eea8d336e3b1d7fe7fa0509edbd
dickirby <danyasolod2004@gmail.com>
2023-11-12 18:38:44 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,27 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divCtrls = document.querySelector("#controls");
+const containerBox = document.querySelector("#boxes");
+function createBoxes(amount) {
+  containerBox.innerHTML = "";
+  amount = Number(divCtrls.querySelector("input").value);
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const element = amount[i];
+    const divelem = document.createElement("div");
+    divelem.style.height = size + "px";
+    divelem.style.width = size + "px";
+    divelem.style.backgroundColor = getRandomHexColor();
+    containerBox.appendChild(divelem);
+    size += 10;
+  }
+}
+function destroyBoxes() {
+  containerBox.innerHTML = "";
+}
+divCtrls.querySelector("[data-create]").addEventListener("click", createBoxes);
+divCtrls
+  .querySelector("[data-destroy]")
+  .addEventListener("click", destroyBoxes);
